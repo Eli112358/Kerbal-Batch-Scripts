cddf00789d23e7d988a712848673a0ff9aedcd84
Eli112358 <Eli112358.SARP@hotmail.com>
2016-07-26 15:13:39 -0400

    Fixed a bug by converting relative paths to full paths

--- KspProfiles.cmd
+++ KspProfiles.cmd
@@ -16,33 +16,33 @@ exit/b
 set "kspProfiles=%~0dp"
 setx kspProfiles "%kspProfiles%"
 if defined kspInstall goto setupFolders
-for "delims=;" %%I in (\Program Files;\Program Files (x86)) do if exist %%I (pushd %%I & call :setupEnv %%I & popd)
+for "delims=;" %%I in (C:\Program Files;C:\Program Files (x86)) do if exist "%%I" (pushd "%%I" & call :setupEnv "%%I" & popd)
 setx kspInstall "%kspInstall%"
 path %path%;%kspProfiles%
 :setupFolders
 for %%I in (GameData saves) do move "%kspInstall%\%%I" 2>>%kspProfilesLogs%
 call :create Vanilla
-for /f %%I in ('dir/b GameData ^2>>%kspProfilesLogs%') do if not %%I==Squad call :create Modded
-if exist Profiles\Modded (( for /f %%I in ('dir/b/a:d GameData ^2>>%kspProfilesLogs%') do call :addMod Modded %%I) & call :addModuleManager Modded )
+for /f %%I in ('dir/b "%kspProfiles%\GameData" ^2>>%kspProfilesLogs%') do if not %%I==Squad call :create Modded
+if exist "%kspProfiles%\Profiles\Modded" (( for /f %%I in ('dir/b/a:d "%kspProfiles%\GameData" ^2>>%kspProfilesLogs%') do call :addMod Modded %%I) & call :addModuleManager Modded )
 exit/b
 :setupEnv
 pushd %1
-for /f "delims=" %%I in ('dir/b/s/a:d "Kerbal Space Program" 2^>>%kspProfilesLogs%') do set "kspInstall=%%~I"
+for /f "delims=" %%I in ('dir/b/s/a:d "Kerbal Space Program" 2^>>%kspProfilesLogs%') do set "kspInstall=%%~fI"
 popd
 exit/b
 :create
-md Profiles\%1 2>>%kspProfilesLogs%
-for %%J in (GameData saves) do md Profiles\%1\%%J 2>>%kspProfilesLogs%
-mklink/d Profiles\%%I\GameData\Squad 2>>%kspProfilesLogs%
-for %%J in (senarios training) do mklink/d Profiles\%1\saves\%%J saves\%%J 2>>%kspProfilesLogs%
+md "%kspProfiles%\Profiles\%1" 2>>%kspProfilesLogs%
+for %%J in (GameData saves) do md "%kspProfiles%\Profiles\%1\%%J" 2>>%kspProfilesLogs%
+mklink/d "%kspProfiles%\Profiles\%%I\GameData\Squad" "%kspProfiles%\GameData\Squad" 2>>%kspProfilesLogs%
+for %%J in (senarios training) do mklink/d "%kspProfiles%\Profiles\%1\saves\%%J" "%kspProfiles%\saves\%%J" 2>>%kspProfilesLogs%
 exit/b
 :addMod
-mklink/d Profiles\%1\GameData\%2 GameData\%2 2>>%kspProfilesLogs%
+mklink/d "%kspProfiles%\Profiles\%1\GameData\%2" "%kspProfiles%\GameData\%2" 2>>%kspProfilesLogs%
 exit/b
 :addModuleManager
-if not exist GameData\ModuleManager.*.dll exit/b
-for /f %%I in ('dir/b/a:-d GameData\ModuleManager.*.dll ^2>>%kspProfilesLogs%') do set MMVersion=%%I
-mklink/h Profiles\%1\GameData\%MMVersion% GameData\%MMVersion% 2>>%kspProfilesLogs%
+if not exist "%kspProfiles%\GameData\ModuleManager.*.dll" exit/b
+for /f %%I in ('dir/b/a:-d "%kspProfiles%\GameData\ModuleManager.*.dll" ^2>>%kspProfilesLogs%') do set MMVersion=%%I
+mklink/h "%kspProfiles%\Profiles\%1\GameData\%MMVersion%" "%kspProfiles%\GameData\%MMVersion%" 2>>%kspProfilesLogs%
 exit/b
 :activate
 pushd "%kspInstall%"
